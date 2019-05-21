16c5569ad24567de247a19f69f4fea1615398547
siva k <siva.k@in.abb.com>
2019-05-21 16:59:45 +0530

    initial commit

---
+++ paytm_java/src/paytm_java/PaytmConstants.java
@@ -0,0 +1,15 @@
+package paytm_java;
+
+public class PaytmConstants {
+	// public final static String MID="WorldP64425807474247";
+	// public final static String MERCHANT_KEY="kbzk1DSbJiV_O3p5";
+
+	public final static String MID = "IMuStc22363784244148";
+
+	public final static String MERCHANT_KEY = "9Jc9l4rglJ!Dqxhq";
+	public final static String INDUSTRY_TYPE_ID = "Retail";
+	public final static String CHANNEL_ID = "WAP";
+	public final static String WEBSITE = "APPSTAGING";
+	public final static String PAYTM_URL = "https://securegw-stage.paytm.in/theia/processTransaction";
+
+}
